d1b3892d8b10781e2f18998a707d2ef47b7fef3b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-08 15:45:20 +0530

    inno2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NAv from './components/Navbar/NAv';
+import Home from './components/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
     <NAv/>      
+    <Home/>
     </div>
   );
 }
