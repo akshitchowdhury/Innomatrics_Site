af51927a5e83b6af1388b2493ff5a52c8f3ad770
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-08 16:05:03 +0530

    inno3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 
 const Home = () => {
-  return (
-    <div className='home'>
+  
+  
+    const[isAnimating, setIsAnimating] = useState(true);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsAnimating(prevState => !prevState); // Toggle animation state
+        }, 1000); // Adjust the delay according to your preference (in milliseconds)
+
+        // Cleanup function to clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []); // Empty dependency array ensures this effect runs only once on mount
+    return (
+    <div className={`home ${isAnimating? 'animate' : '' }`}  >
 
 
 
