0a4c724d618cb5f6ef1e604ff5af7ca845dc8efa
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-17 18:16:01 +0530

    buttonChanges

--- src/components/Navbar/NAv.jsx
+++ src/components/Navbar/NAv.jsx
@@ -31,7 +31,7 @@ function NAv() {
             <button
               type="button"
               className="text-white 
-              bg-blue-700 focus:ring-4 
+              bg-zinc-700 focus:ring-4 
               focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2
               text-center dark:bg-blue-600 dark:focus:ring-blue-800
               hover:bg-zinc-800 hover:transform hover:scale-105
