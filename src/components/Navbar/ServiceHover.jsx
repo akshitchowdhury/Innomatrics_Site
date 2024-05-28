0d099e53fd676a0474cbacf467b268e521636741
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-05-28 12:46:48 +0530

    portfolioPage2

--- src/components/Navbar/ServiceHover.jsx
+++ src/components/Navbar/ServiceHover.jsx
@@ -30,7 +30,8 @@ const ServiceHover = () => {
   return (
     <li
       className="relative serviceBlock text-white rounded
-       hover:bg-gray-100 md:hover:bg-transparent md:p-0 md:hover:text-zinc-900 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        
+            transition ease-in-out duration-300md:hover:bg-transparent md:p-0 md:hover:text-zinc-900 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       style={{
@@ -49,19 +50,24 @@ const ServiceHover = () => {
           hover:scale-100 transition ease-in-out duration-300
           text-sm"
           >
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <li className="px-4 py-2 hover:bg-sky-500 
+            transition ease-in-out duration-300 dark:hover:bg-gray-700">
               <Link to="/appdev">Application Development</Link>
             </li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <li className="px-4 py-2 hover:bg-sky-500 
+            transition ease-in-out duration-300dark:hover:bg-gray-700">
               <Link to="/webdev">Website Development</Link>
             </li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <li className="px-4 py-2 hover:bg-sky-500 
+            transition ease-in-out duration-300dark:hover:bg-gray-700">
               <Link to="/webdesign">Website Design</Link>
             </li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <li className="px-4 py-2 hover:bg-sky-500 
+            transition ease-in-out duration-300dark:hover:bg-gray-700">
               <Link to="/digitalmarket">Digital Marketing</Link>
             </li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <li className="px-4 py-2 hover:bg-sky-500 
+            transition ease-in-out duration-300dark:hover:bg-gray-700">
               <Link to="/socialmedia">Social Media Marketing</Link>
             </li>
           </ul>
